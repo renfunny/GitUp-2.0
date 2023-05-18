f00f9ea003f9559570d996ecd07cd495b18fadb3
Renato Cordova <ren99cordova@gmail.com>
2023-05-17 20:45:05 -0400

    will use previus AI Summarizer code as a starting point for this project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/renfunny/AI-Summarizer", "_blank")
+          }
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Sumz, an open-source article summarizer that
+        transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
